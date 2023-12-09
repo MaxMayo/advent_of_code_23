a194426e9e93c06c3ae28935e5dcf61cfdfd5262
jordon <jordonboswelldeveloper@gmail.com>
2023-12-09 15:34:30 +0000

    part 1 complete

--- src/main/java/com/example/adventofcode2023/day_9/NextInSequence.java
+++ src/main/java/com/example/adventofcode2023/day_9/NextInSequence.java
@@ -2,5 +2,55 @@ package com.example.adventofcode2023.day_9;
 
 import com.example.adventofcode2023.common.TestDriven;
 
-public class NextInSequence implements TestDriven<> {
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NextInSequence implements TestDriven<Long> {
+
+    @Override
+    public Long runPartOne(BufferedReader bufferedReader) {
+        return bufferedReader.lines()
+                .map(line -> line.split("\\s"))
+                .map(
+                        stringArray -> Arrays.stream(stringArray)
+                                .map(Long::parseLong).toList()
+                )
+                .map(NextInSequence::findSequences)
+                .map(listOfSequences -> listOfSequences
+                        .reversed()
+                        .stream()
+                        .map(List::getLast)
+                        .reduce(0L, Long::sum)
+                )
+                .reduce(0L, Long::sum);
+    }
+
+    private static List<List<Long>> findSequences(List<Long> firstRow) {
+        int maxDepth = firstRow.size();
+        List<List<Long>> sequences = new ArrayList<>(maxDepth);
+        List<Long> currentSequence = firstRow;
+        for (int depth = 1; depth <= maxDepth; depth += 1) {
+            int currentSequenceLength = currentSequence.size();
+            List<Long> sequenceDifferences = new ArrayList<>(currentSequence.size() - 1);
+            boolean allAre0 = true;
+            for (int i = 0; i < currentSequenceLength - 1; i++) {
+                long difference = currentSequence.get(i + 1) - currentSequence.get(i);
+                sequenceDifferences.add(difference);
+                if (difference != 0) allAre0 = false;
+            }
+            sequences.add(sequenceDifferences);
+            if (allAre0) break;
+            currentSequence = sequenceDifferences;
+        }
+        sequences.add(firstRow);
+        return sequences;
+    }
+
+
+    @Override
+    public Long runPartTwo(BufferedReader bufferedReader) {
+        return null;
+    }
 }
---
+++ src/test/java/com/example/adventofcode2023/day_9/NextInSequenceTest.java
@@ -0,0 +1,48 @@
+package com.example.adventofcode2023.day_9;
+
+import com.example.adventofcode2023.common.TestDriven;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NextInSequenceTest {
+
+    File file;
+    TestDriven<Long> nextInSequence;
+
+    @BeforeEach
+    void setup() throws FileNotFoundException {
+        file = ResourceUtils.getFile("classpath:day_9/example.txt");
+        nextInSequence = new NextInSequence();
+    }
+    @Test
+    @SneakyThrows
+    void testDrivenRun() throws IOException {
+        var result = nextInSequence.testDrivenRun(file);
+        assertEquals(114, result);
+    }
+
+    @Test
+    @SneakyThrows
+    void realRun() {
+        file = ResourceUtils.getFile("classpath:day_9/input.txt");
+        var result = nextInSequence.testDrivenRun(file);
+        System.out.println(result);
+        assertNotEquals(927219961, result);
+        assertEquals(1938800261, result);
+    }
+
+    @Test
+    @SneakyThrows
+    void testDrivenRun2() {
+        var result = nextInSequence.testDrivenRun2(file);
+        assertEquals(2, result);
+    }
+}
